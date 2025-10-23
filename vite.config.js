5afe6f21ed146613d5eb54038602c83e14614315
yli0703 <yli0703@student.monash.edu>
2025-10-24 01:57:29 +1100

    debug

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [vue()],
+  base: '/NoMash-Library/',  
 })
